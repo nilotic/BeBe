3e17ca149abc13fd28b58855bfa3f88f33ff3d72
Den Jo <nilotic2@gmail.com>
2021-04-12 19:48:05 +0900

    Updated the Button Style1

--- BeBe/Component/Button/ButtonStyle1.swift
+++ BeBe/Component/Button/ButtonStyle1.swift
@@ -25,6 +25,7 @@ struct ButtonStyle1: ButtonStyle {
             }
             .frame(width: 0, height: 0)
             .onPreferenceChange(FramePreferenceKey.self) { frame in
+                guard !data.isAppeared else { return }
                 data.isAppeared = 0 <= frame.origin.x
             }
             
@@ -32,14 +33,7 @@ struct ButtonStyle1: ButtonStyle {
             configuration.label
                 .opacity(configuration.isPressed ? 0.5 : 1)
                 .scaleEffect(configuration.isPressed ? 0.92 : 1)
-                .animation(.easeInOut(duration: data.isAppeared ? 0.18 : 0))
+                .animation(data.isAppeared ? .easeInOut(duration: 0.18) : nil)
         }
     }
-}
-
-final class ButtonStyle1Data: ObservableObject {
-    
-    // MARK: - Value
-    // MARK: Public
-    @Published var isAppeared = false
 }
